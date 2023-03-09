e86fa871b10d6b7ed6044aa27535cfdcffaf282e
Malphatt <Ocelot.mf@gmail.com>
2023-03-09 12:45:34 +0000

    Task 3 Complete
    3. Search the selected Array for a user-defined value, if the value exists, then provide its location (if it appears more than once then provide ALL the locations) otherwise provide an error message.

--- Program.cs
+++ Program.cs
@@ -2,37 +2,96 @@
 
     class Program {
 
+        // Create a new instance of the Roads class
+        static Roads roads = new Roads();
+
         static void Main(string[] args) {
-            // Create a new instance of the Roads class
-            Roads roads = new Roads();
 
             // Display the 10th element of each road in ascending order
-            foreach (String element in roads.Display10SortAscending("Road_1_256")) {
+            foreach (String element in roads.Display10Elements("Road_1_256", true)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
-            foreach (String element in roads.Display10SortAscending("Road_2_256")) {
+            foreach (String element in roads.Display10Elements("Road_2_256", true)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
-            foreach (String element in roads.Display10SortAscending("Road_3_256")) {
+            foreach (String element in roads.Display10Elements("Road_3_256", true)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
 
             // Display the 10th element of each road in descending order
-            foreach (String element in roads.Display10SortDescending("Road_1_256")) {
+            foreach (String element in roads.Display10Elements("Road_1_256", false)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
-            foreach (String element in roads.Display10SortDescending("Road_2_256")) {
+            foreach (String element in roads.Display10Elements("Road_2_256", false)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
-            foreach (String element in roads.Display10SortDescending("Road_3_256")) {
+            foreach (String element in roads.Display10Elements("Road_3_256", false)) {
                 Console.WriteLine(element);
             }
             Console.WriteLine();
+
+            inputRoad();
+
+        }
+
+        static void inputRoad() { // Display the location of a user-defined element in a user-defined road
+            
+            bool inputRoad = false; // This variable is used to check if the user has entered a valid road name
+            while (!inputRoad) { // While the user has not entered a valid road name
+
+                Console.Write("Enter a road name: ");
+                String? road = Console.ReadLine();
+
+                if (road == null || road == "") {
+
+                    Console.WriteLine("Invalid road name");
+                    continue;
+
+                } else
+                if (road == "Road_1_256" || road == "Road_2_256" || road == "Road_3_256") { // If the road name is valid
+                    
+                    inputRoad = true; // Set the inputRoad variable to true
+                    findElement(road); // Call the findElement method
+                    
+                } else { // If the road name is invalid
+                    Console.WriteLine("Invalid road name");
+                }
+            }
+        }
+
+        static void findElement(String road) {
+                
+                bool inputElement = false; // This variable is used to check if the user has entered a valid element
+                while (!inputElement) { // While the user has not entered a valid element
+    
+                    Console.Write("Enter an element: ");
+                    String? element = Console.ReadLine();
+    
+                    if (element == null || element == "") {
+    
+                        Console.WriteLine("Invalid element");
+                        continue;
+    
+                    } else {
+                        inputElement = true; // Set the inputElement variable to true
+                        String[] foundElement = roads.FindElement(road, element); // Call the FindElement method
+                        if (foundElement.Length == 0) { // If the element was not found
+
+                            Console.WriteLine("Element not found"); // Display an error message
+                            continue;
+                            
+                        } else {
+                            foreach (String elementLocation in foundElement) { // Display the location of the element
+                                Console.WriteLine("Element: " + element + " | Found At Location: " + elementLocation);
+                            }
+                        }
+                    }
+                }
         }
     }
 }
--- Roads.cs
+++ Roads.cs
@@ -24,103 +24,113 @@ namespace CMP1124M_OOP {
             Road_3_256 = System.IO.File.ReadAllLines("Roads/Road_3_256.txt");
         }
 
-        public String[] Display10SortAscending(String road) { // This method returns a string array of every 10th element of the road in ascending order
+        public String[] SortAsc(String road) { // This method returns a string array of every 10th element of the road in ascending order
             
+            String[] selectedRoad;
+
             if (road == "Road_1_256") { // If the road is Road_1_256
-
-                String[] sortedRoad = _Road_1_256;
-                String[] returnRoad = new String[_Road_1_256.Length / 10];
-                
-                Array.Sort(sortedRoad); // Sort the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
-                }
-
-                return returnRoad;
+                selectedRoad = _Road_1_256;
             } else
             if (road == "Road_2_256") { // If the road is Road_2_256
-
-                String[] sortedRoad = _Road_2_256;
-                String[] returnRoad = new String[_Road_2_256.Length / 10];
-
-                Array.Sort(sortedRoad); // Sort the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
-                }
-
-                return returnRoad;
-
+                selectedRoad = _Road_2_256;
             } else
             if (road == "Road_3_256") { // If the road is Road_3_256
-
-                String[] sortedRoad = _Road_3_256;
-                String[] returnRoad = new String[_Road_3_256.Length / 10];
-
-                Array.Sort(sortedRoad); // Sort the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
-                }
-
-                return returnRoad;
-
-            } else { // If the road name is invalid
-                Console.WriteLine("Invalid road name");
-                return new String[0];
+                selectedRoad = _Road_3_256;
+            } else {
+                return new String[0]; // Return an empty array
             }
+
+            Array.Sort(selectedRoad); // Sort the road
+
+            return selectedRoad;
         }
 
-        public String[] Display10SortDescending(String road) { // This method returns a string array of every 10th element of the road in descending order
+        String[] SortDesc(String road) { // This method returns a string array of every 10th element of the road in descending order
+            
+            String[] selectedRoad;
+
+            if (road == "Road_1_256") { // If the road is Road_1_256
+                selectedRoad = _Road_1_256;
+            } else
+            if (road == "Road_2_256") { // If the road is Road_2_256
+                selectedRoad = _Road_2_256;
+            } else
+            if (road == "Road_3_256") { // If the road is Road_3_256
+                selectedRoad = _Road_3_256;
+            } else {
+                return new String[0]; // Return an empty array
+            }
+
+            String[] sortedRoad = SortAsc(road); // Sort the road in ascending order
+            Array.Reverse(sortedRoad);  // Reverse the road
+
+            return sortedRoad;
+        }
+
+        public String[] Display10Elements(String road, bool ascending) { // This method returns a string array of every 10th element of the road in ascending or descending order
+
+            List<String> returnRoad = new List<String>();
+            String[] sortedRoad; // Sort the road in descending order
+            
+            if (ascending) { // If the road is to be sorted in ascending order
+                sortedRoad = SortAsc(road); // Sort the road in ascending order
+            } else { // If the road is to be sorted in descending order
+                sortedRoad = SortDesc(road); // Sort the road in descending order
+            }
+
+            for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
+                returnRoad.Add(sortedRoad[i]);
+            }
+
+            return returnRoad.ToArray();
+        }
+
+        public String[] FindElement(String road, String searchValue) { // This method returns the location of the value in the road
 
             if (road == "Road_1_256") { // If the road is Road_1_256
 
-                String[] sortedRoad = _Road_1_256;
-                String[] returnRoad = new String[_Road_1_256.Length / 10];
+                List<String> locationList = new List<String>(); // Create a list to store the locations
 
-                Array.Sort(sortedRoad);     // Sort the road
-                Array.Reverse(sortedRoad);  // Reverse the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
+                // Loop through the road to find the locations at which the value is found and add them to the list
+                for (int location = 0; location < _Road_1_256.Length; location++) {
+                    if (_Road_1_256[location] == searchValue) {
+                        locationList.Add(location.ToString()); // Add the location to the list
+                    }
                 }
 
-                return returnRoad;
+                return locationList.ToArray(); // Convert the list to an array and return it
 
             } else
             if (road == "Road_2_256") { // If the road is Road_2_256
 
-                String[] sortedRoad = _Road_2_256;
-                String[] returnRoad = new String[_Road_2_256.Length / 10];
+                List<String> locationList = new List<String>(); // Create a list to store the locations
 
-                Array.Sort(sortedRoad);     // Sort the road
-                Array.Reverse(sortedRoad);  // Reverse the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
+                // Loop through the road to find the locations at which the value is found and add them to the list
+                for (int location = 0; location < _Road_2_256.Length; location++) {
+                    if (_Road_2_256[location] == searchValue) {
+                        locationList.Add(location.ToString()); // Add the location to the list
+                    }
                 }
 
-                return returnRoad;
+                return locationList.ToArray(); // Convert the list to an array and return it
 
             } else
             if (road == "Road_3_256") { // If the road is Road_3_256
 
-                String[] sortedRoad = _Road_3_256;
-                String[] returnRoad = new String[_Road_3_256.Length / 10];
+                List<String> locationList = new List<String>(); // Create a list to store the locations
 
-                Array.Sort(sortedRoad);     // Sort the road
-                Array.Reverse(sortedRoad);  // Reverse the road
-
-                for (int i = 0; i < sortedRoad.Length; i += 10) { // Add every 10th element to the returnRoad array
-                    returnRoad[i / 10] = sortedRoad[i];
+                // Loop through the road to find the locations at which the value is found and add them to the list
+                for (int location = 0; location < _Road_3_256.Length; location++) {
+                    if (_Road_3_256[location] == searchValue) {
+                        locationList.Add(location.ToString()); // Add the location to the list
+                    }
                 }
 
-                return returnRoad;
+                return locationList.ToArray(); // Convert the list to an array and return it
 
             } else { // If the road name is invalid
                 Console.WriteLine("Invalid road name");
-                return new String[0];
+                return new String[0]; // Return an empty array
             }
         }
     }
