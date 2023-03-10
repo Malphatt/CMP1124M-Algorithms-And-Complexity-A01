08fe815b5a9591b5a7d944a4a423b188a5d9e62d
Malphatt <Ocelot.mf@gmail.com>
2023-03-10 15:30:00 +0000

    Added "Instructions For Use" to Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,14 @@
 
     class Program {
         
-
+        // ===========================
+        // === Instructions of Use ===
+        // ===========================
+        //
+        // How To Run:
+        // 1. Open a new terminal by pressing CTRL + SHIFT + ' (apostrophe) {Or by navigating to Terminal -> New Terminal}
+        // 2. Type 'dotnet run' and press ENTER
+        // 3. The program will run and you will be able to interact with it
 
         static void Main(string[] args) {
 
